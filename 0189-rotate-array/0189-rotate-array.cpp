a5ef9485dafd8efa418f20f875ce4f0e1dcff986
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2025-11-15 22:34:35 +0530

    Time: 0 ms (100%), Space: 29.4 MB (83.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,10 +4,8 @@ public:
         int n = nums.size();
         k = k % n;
 
-        vector<int> temp(n);
-        for(int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-        nums = temp;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };

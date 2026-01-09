5d88f72b1a22d84732cf95e37b05717459d78de2
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-01-09 23:57:05 +0530

    Time: 0 ms (100%), Space: 16.7 MB (12.01%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -10,23 +10,38 @@
  * };
  */
 class Solution {
-    pair<TreeNode*, int> dfs(TreeNode* root) {
-        if (!root) return {nullptr, 0};
-
-        auto left = dfs(root->left);
-        auto right = dfs(root->right);
-
-        if (left.second > right.second)
-            return {left.first, left.second + 1};
-        if (right.second > left.second)
-            return {right.first, right.second + 1};
-
-        // equal depth
-        return {root, left.second + 1};
-    }
-
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        return dfs(root).first;
+        unordered_map<TreeNode*, TreeNode*> parent;
+        vector<TreeNode*> level;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            level.clear();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front(); q.pop();
+                level.push_back(node);
+                if (node->left) {
+                    parent[node->left] = node;
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    parent[node->right] = node;
+                    q.push(node->right);
+                }
+            }
+        }
+
+        unordered_set<TreeNode*> s(level.begin(), level.end());
+        while (s.size() > 1) {
+            unordered_set<TreeNode*> next;
+            for (auto node : s)
+                next.insert(parent[node]);
+            s = move(next);
+        }
+
+        return *s.begin();
     }
 };

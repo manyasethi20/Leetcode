98bee986928905bacc18eda8ac473fde4b271abf
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-01-29 10:05:47 +0530

    Time: 68 ms (25.87%), Space: 100.2 MB (70.9%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -2,64 +2,39 @@ class Solution {
 public:
     long long minimumCost(string source, string target, vector<char>& original,
                           vector<char>& changed, vector<int>& cost) {
-        vector<vector<pair<int, int>>> adjacencyList(26);
-
-        int conversionCount = original.size();
-        for (int i = 0; i < conversionCount; i++) {
-            adjacencyList[original[i] - 'a'].push_back(
-                {changed[i] - 'a', cost[i]});
-        }
-
-        vector<vector<long long>> minConversionCosts(26, vector<long long>(26));
-        for (int i = 0; i < 26; i++) {
-            minConversionCosts[i] = dijkstra(i, adjacencyList);
-        }
-
         long long totalCost = 0;
-        int stringLength = source.length();
-        for (int i = 0; i < stringLength; i++) {
-            if (source[i] != target[i]) {
-                long long charConversionCost =
-                    minConversionCosts[source[i] - 'a'][target[i] - 'a'];
-                if (charConversionCost == -1) {
-                    return -1;
+
+        vector<vector<long long>> minCost(26, vector<long long>(26, INT_MAX));
+
+        for (int i = 0; i < original.size(); ++i) {
+            int startChar = original[i] - 'a';
+            int endChar = changed[i] - 'a';
+            minCost[startChar][endChar] =
+                min(minCost[startChar][endChar], (long long)cost[i]);
+        }
+
+        for (int k = 0; k < 26; ++k) {
+            for (int i = 0; i < 26; ++i) {
+                for (int j = 0; j < 26; ++j) {
+                    minCost[i][j] =
+                        min(minCost[i][j], minCost[i][k] + minCost[k][j]);
                 }
-                totalCost += charConversionCost;
             }
         }
-        return totalCost;
-    }
 
-private:
-    vector<long long> dijkstra(
-        int startChar, const vector<vector<pair<int, int>>>& adjacencyList) {
-
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                       greater<pair<long long, int>>> priorityQueue;
-
-        priorityQueue.push({0, startChar});
-
-        vector<long long> minCosts(26, -1);
-
-        while (!priorityQueue.empty()) {
-            auto [currentCost, currentChar] = priorityQueue.top();
-            priorityQueue.pop();
-
-            if (minCosts[currentChar] != -1 &&
-                minCosts[currentChar] < currentCost)
+        for (int i = 0; i < source.size(); ++i) {
+            if (source[i] == target[i]) {
                 continue;
-
-            for (auto& [targetChar, conversionCost] :
-                 adjacencyList[currentChar]) {
-                long long newTotalCost = currentCost + conversionCost;
-
-                if (minCosts[targetChar] == -1 ||
-                    newTotalCost < minCosts[targetChar]) {
-                    minCosts[targetChar] = newTotalCost;
-                    priorityQueue.push({newTotalCost, targetChar});
-                }
             }
+            int sourceChar = source[i] - 'a';
+            int targetChar = target[i] - 'a';
+
+            if (minCost[sourceChar][targetChar] >= INT_MAX) {
+                return -1;
+            }
+            totalCost += minCost[sourceChar][targetChar];
         }
-        return minCosts;
+
+        return totalCost;
     }
 };

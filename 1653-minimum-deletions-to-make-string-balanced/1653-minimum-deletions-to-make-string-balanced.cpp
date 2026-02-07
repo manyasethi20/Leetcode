a8541f30f25fb6cdc54401a25834fa881ff589f4
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-02-07 18:14:45 +0530

    Time: 48 ms (23.22%), Space: 55.1 MB (20.86%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int bCount = 0, deletions = 0;
+        int n = s.length();
+        vector<int> count_a(n, 0);
+        vector<int> count_b(n, 0);
+        int b_count = 0;
 
-        for (char c : s) {
-            if (c == 'b') {
-                bCount++;
-            } else { 
-                deletions = min(deletions + 1, bCount);
-            }
+        for (int i = 0; i < n; i++) {
+            count_b[i] = b_count;
+            if (s[i] == 'b') b_count++;
         }
-        return deletions;
+
+        int a_count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            count_a[i] = a_count;
+            if (s[i] == 'a') a_count++;
+        }
+
+        int min_deletions = n;
+        for (int i = 0; i < n; i++) {
+            min_deletions = min(min_deletions, count_a[i] + count_b[i]);
+        }
+
+        return min_deletions;
     }
 };

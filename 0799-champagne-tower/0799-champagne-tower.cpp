3b7f502b832ecc9f98f22785bc41d12755fd5101
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-02-14 19:20:50 +0530

    Time: 0 ms (100%), Space: 9.8 MB (77.48%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(101, vector<double>(101, 0.0));
-        dp[0][0] = poured;
+        vector<double> dp(query_row + 2);
+        dp[0] = poured;
 
         for(int r = 0; r < query_row; r++){
-            for(int c = 0; c <= r; c++){
-                double overflow = max(0.0, dp[r][c] - 1);
-                if(overflow > 0){
-                    dp[r+1][c] += overflow / 2;
-                    dp[r+1][c+1] += overflow / 2;
-                }
+            for(int c = r; c >= 0; c--){
+                double overflow = max(0.0, dp[c] - 1);
+                dp[c] = overflow / 2;
+                dp[c+1] += overflow / 2;
             }
         }
-        return min(1.0, dp[query_row][query_glass]);
+        return min(1.0, dp[query_glass]);
     }
 };

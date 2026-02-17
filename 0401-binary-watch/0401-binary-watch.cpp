7f852e16e080ff565dc2d06247db2a0cd429e8e3
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-02-17 19:09:49 +0530

    Time: 0 ms (100%), Space: 9.8 MB (41.32%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+
+                int hourBits = __builtin_popcount(hour);
+                int minuteBits = __builtin_popcount(minute);
+
+                if (hourBits + minuteBits == turnedOn) {
+                    string time = to_string(hour) + ":";
+                    if (minute < 10)
+                        time += "0";
+                    time += to_string(minute);
+                    result.push_back(time);
+                }
+            }
+        }
+        return result;
+    }
+};

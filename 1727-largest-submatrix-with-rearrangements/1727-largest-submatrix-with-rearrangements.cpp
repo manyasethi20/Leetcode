e490a4a56dd63b797f5be87ccc859cf242c1fe28
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-03-17 22:26:24 +0530

    Time: 62 ms (21.28%), Space: 99.1 MB (17.55%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -3,22 +3,26 @@ public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        vector<int> prevRow = vector(n, 0);
         int ans = 0;
         
         for (int row = 0; row < m; row++) {
+            vector<int> currRow = matrix[row];
             for (int col = 0; col < n; col++) {
-                if (matrix[row][col] != 0 && row > 0) {
-                    matrix[row][col] += matrix[row - 1][col];
+                if (currRow[col] != 0) {
+                    currRow[col] += prevRow[col];
                 }
             }
             
-            vector<int> currRow = matrix[row];
-            sort(currRow.begin(), currRow.end(), greater());
+            vector<int> sortedRow = currRow;
+            sort(sortedRow.begin(), sortedRow.end(), greater());
             for (int i = 0; i < n; i++) {
-                ans = max(ans, currRow[i] * (i + 1));
+                ans = max(ans, sortedRow[i] * (i + 1));
             }
+            
+            prevRow = currRow;
         }
-        
+
         return ans;
     }
 };

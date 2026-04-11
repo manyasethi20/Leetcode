a5b89fe2344be1aa7abd12d348b1249c34918b49
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-04-11 23:08:31 +0530

    Time: 5 ms (99.25%), Space: 220.9 MB (99.25%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int minimumDistance(vector<int>& nums) {
-        int n = nums.size();
-        std::vector<int> next(n, -1);
-        std::unordered_map<int, int> occur;
-        int ans = n + 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (occur.count(nums[i])) {
-                next[i] = occur[nums[i]];
+    using int2=array<int, 2>;
+    static int minimumDistance(vector<int>& nums) {
+        const int n=nums.size(), M=*max_element(nums.begin(), nums.end());
+        const int2 none={-1, -1};
+        vector<int2> pos(M+1, none);
+        int ans=INT_MAX;
+        for(int i=0; i<n; i++){
+            const int x=nums[i];
+            if (pos[x][1]!=-1){
+                ans=min(ans, (i-pos[x][1])<<1);
             }
-            occur[nums[i]] = i;
+            pos[x][1]=exchange(pos[x][0], i);
         }
-
-        for (int i = 0; i < n; i++) {
-            int secondPos = next[i];
-            if (secondPos != -1) {
-                int thirdPos = next[secondPos];
-                if (thirdPos != -1) {
-                    ans = std::min(ans, thirdPos - i);
-                }
-            }
-        }
-
-        return ans == n + 1 ? -1 : ans * 2;
+        return ans==INT_MAX?-1:ans;
     }
 };

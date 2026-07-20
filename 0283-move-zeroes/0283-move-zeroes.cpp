4a7fbbbc0a92d6c60bca8580958a1e95b60ee208
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-07-21 03:06:09 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.43%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int j = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] != 0) {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        while(j < arr.size()) {
+            arr[j] = 0;
+            j++;
+        }
+    }
+};

46c877e6cad3edc7f3b1e06f2b0f4aa9d947a2a1
Manya Sethi <160034294+manyasethi20@users.noreply.github.com>
2026-08-08 19:23:03 +0530

    Time: 0 ms (100%), Space: 14 MB (23.73%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -4,7 +4,14 @@ public:
         int m = mat.size();
         int n = mat[0].size();
 
-        vector<vector<int>> answer(m, vector<int>(n, 0));
+        vector<vector<int>> prefix(m + 1, vector<int>(n + 1, 0));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                prefix[i + 1][j + 1] = mat[i][j] + prefix[i][j + 1] + prefix[i + 1][j] - prefix[i][j];
+            }
+        }
+         vector<vector<int>> answer(m, vector<int>(n, 0));
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
@@ -16,12 +23,12 @@ public:
                 int startCol = max(0, j - k);
                 int endCol = min(n - 1, j + k);
 
-                for(int r = startRow; r <= endRow; r++) {
-                    for(int c = startCol; c <= endCol; c++) {
-                        sum += mat[r][c];
-                    }
-                }
-                answer[i][j] = sum;
+                int whole = prefix[endRow + 1][endCol + 1];
+                int top = prefix[startRow][endCol + 1];
+                int left = prefix[endRow + 1][startCol];
+                int overlap = prefix[startRow][startCol];
+
+                answer[i][j] = whole - top - left + overlap;
             }
         }
         return answer;
